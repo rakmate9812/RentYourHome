3985d95a53337c9a6433f52314c2a4843fedd449
GaborToth22 <toccs22@gmail.com>
2024-01-18 10:18:44 +0100

    fix: the response.json() changed to response.text()

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,15 +2,13 @@ import React, { useEffect, useState } from 'react';
 
 function App() {
 
-  const [data, setData] = useState(null);
+  const [data, setData] = useState("null");
   useEffect(() => {
-    fetch('http://localhost:5256/User/users', {
-      method: 'GET',
-    })
-      .then(response => response.json())
-      .then(data => {
-        setData(data);
-        console.log(data);
+    fetch('http://localhost:5256/api/users')
+      .then(response => response.text())
+      .then(d => {
+        setData(d);
+        console.log(d);
       }
       )
       .catch(error => console.log(error))
